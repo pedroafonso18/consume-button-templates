1e626f1308b062d6d457addfd8fb02315d6e6c31
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-07-14 11:44:55 -0300

    fix: AGAIN

--- src/process/process.rs
+++ src/process/process.rs
@@ -154,7 +154,7 @@ pub async fn process_webhook(
     let conn_tuple = (conn, source);
     if button_text_lwr.contains("sim") || button_text_lwr.contains("chamar") {
         crate::api::api::send_gupshup_message(api_key_gup, "Vamos lá! Antes de realizar a consulta, é importante saber: o empréstimo do Bolsa Família pode chegar até R$650, caso o seu benefício esteja liberado.\n\nAtualmente, você recebe o Bolsa Família pelo aplicativo Caixa Tem?\n\nDigite:\n1️⃣ Sim\n2️⃣ Não", conn_tuple, &whatsapp_number).await?;
-        match crate::db::insert::insert_log(&db_client_logs, &whatsapp_number, "Perfeito! Agora, você saberia me informar se ainda tem acesso ao aplicativo do FGTS?\n\nDigite: 1 para Sim!\nDigite: 2 para Não!\n", &button_text, "FGTS").await {
+        match crate::db::insert::insert_log(&db_client_logs, &whatsapp_number, "Vamos lá! Antes de realizar a consulta, é importante saber: o empréstimo do Bolsa Família pode chegar até R$650, caso o seu benefício esteja liberado.\n\nAtualmente, você recebe o Bolsa Família pelo aplicativo Caixa Tem?\n\nDigite:\n1️⃣ Sim\n2️⃣ Não\n", &button_text, "BOLSA").await {
             Ok(_) => {
                 info!("Contact creation process completed successfully");
                 Ok(())        
@@ -167,7 +167,7 @@ pub async fn process_webhook(
     } else {
         
         crate::api::api::send_gupshup_message(api_key_gup, "Perfeito! 😊\nAgora, você saberia me informar se ainda tem acesso ao aplicativo do FGTS?\n\nDigite:\n1️⃣ Sim, tenho acesso!\n2️⃣ Não tenho!", conn_tuple, &whatsapp_number).await?;
-        match crate::db::insert::insert_log(&db_client_logs, &whatsapp_number, "Perfeito! Agora, você saberia me informar se ainda tem acesso ao aplicativo do FGTS?\n\nDigite: 1 para Sim!\nDigite: 2 para Não!\n", &button_text, "BOLSA").await {
+        match crate::db::insert::insert_log(&db_client_logs, &whatsapp_number, "Perfeito! Agora, você saberia me informar se ainda tem acesso ao aplicativo do FGTS?\n\nDigite: 1 para Sim!\nDigite: 2 para Não!\n", &button_text, "FGTS").await {
             Ok(_) => {
                 info!("Contact creation process completed successfully");
                 Ok(())        
